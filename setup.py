1567d4a7620c8390cd9333aba93ac03618ccc2a0
Muhammed Saeed <musaeed@tony-4.coli.uni-saarland.de>
2023-11-07 14:28:13 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(),
+    description='A Python package for processing PDTB-style annotations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Muhammed Saeed',
+    author_email='mohammed.yahia3@gmail.com',
+    url='https://github.com/muhammed-saeed/pdtb2.git',
+    install_requires=[
+        # List your package dependencies here
+        # e.g., 'numpy', 'pandas'
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.org/classifiers/
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
